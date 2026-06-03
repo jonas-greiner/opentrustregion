fe06db25d403a4075580416c287754d7f2927a52
Jonas Greiner <jongr@kemi.dtu.dk>
2026-06-03 10:07:03 +0200

    Added missing transport callback function in quasi-Newton header file.

--- extensions/quasi_newton/include/opentrustregion_quasi_newton.h
+++ extensions/quasi_newton/include/opentrustregion_quasi_newton.h
@@ -17,6 +17,10 @@ extern "C" {
  * Declarations for quasi-Newton functions and function pointer types
  * ------------------------------------------------------------------ */
 
+/* Transport callback */
+typedef c_int transport_fn(const c_real* geodesic_c, c_real* transport_vector_c);
+typedef transport_fn* transport_fp;
+
 /* Hessian initialization callback */
 typedef c_int init_hess_fn(c_real* vector_c);
 typedef init_hess_fn* init_hess_fp;
@@ -42,6 +46,7 @@ void init_qn_settings(qn_settings_type* settings);
  * Fortran-callable update_orbs_qn_factory interface
  *
  * @param update_orbs_orig_c_funptr    Pointer to original update_orbs callback
+ * @param transport_c_funptr           C pointer to transport callback
  * @param init_hess_c_funptr           C pointer to init_hess callback
  * @param n_param                      Number of parameters
  * @param settings                     Quasi-Newton settings
@@ -50,6 +55,7 @@ void init_qn_settings(qn_settings_type* settings);
  */
 c_int update_orbs_qn_factory(
     update_orbs_fp update_orbs_orig_c_funptr,
+    transport_fp transport_c_funptr,
     init_hess_fp init_hess_c_funptr,
     c_int n_param,
     qn_settings_type settings,
